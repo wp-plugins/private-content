d983ccbe79b6f32f9c6f71061012a3146d93b774
aldolat <aldolat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-09 14:58:06 +0000

    Releasing version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/private-content/trunk@665632 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- README.md
+++ README.md
@@ -1,39 +1,39 @@
-=== Private Content ===
-Contributors: aldolat, specialk
-Donate link: http://www.aldolat.it/wordpress/wordpress-plugins/private-content/
-Tags: content, private
-Requires at least: 3.0
-Tested up to: 3.5.1
-Stable tag: 2.0
-License: GPLv3 or later
-License URI: http://www.gnu.org/licenses/gpl-3.0.html
+# Private Content #
+**Contributors:** aldolat, specialk  
+**Donate link:** http://www.aldolat.it/wordpress/wordpress-plugins/private-content/  
+**Tags:** content, private  
+**Requires at least:** 3.0  
+**Tested up to:** 3.5.1  
+**Stable tag:** 2.0  
+**License:** GPLv3 or later  
+**License URI:** http://www.gnu.org/licenses/gpl-3.0.html  
 
 Display private post content only to users of a specific role
 
-== Description ==
+## Description ##
 
 This plugin provides a shortcode to display a portion of a post's content only to users of a specific role.
 
 Usage:
 
 Display this text only to Administrators:<br />
-`[private role="administrator"]Text for Administrators[/private]`
+`[private role="administrator"]Text for administrators[/private]`
 
 Display this text only to Administrators and Editors:<br />
-`[private role="editor"]Text for Editors[/private]`
+`[private role="editor"]Text for editors[/private]`
 
 Display this text only to Administrators, Editors, and Authors:<br />
-`[private role="author"]Text for Authors[/private]`
+`[private role="author"]Text for authors[/private]`
 
 Display this text only to Administrators, Editors, Authors, and Contributors:<br />
-`[private role="contributor"]Text for Contributor[/private]`
+`[private role="contributor"]Text for contributor[/private]`
 
 Display this text only to Administrators, Editors, Authors, Contributors, and Subscribers:<br />
-`[private role="subscriber"]Text for Subscribers[/private]`
+`[private role="subscriber"]Text for subscribers[/private]`
 
 Please, note that an Administrator can read an Editor private content or a Subscriber private content, and so on. Same thing for Editor, Author, Contributor, and Subscriber: a higher role can read a lower role content.
 
-If you want to show a note only to a certain role, you have to use a `<role>-only` option.
+If you want to show a note only to a certain role, you have to use a `<role>-only` option.<br />
 In this way, for example, an Administrator or an Editor (roles higher than Author) can't read a note only for Authors.
 
 Here all the cases:
@@ -45,7 +45,6 @@ Here all the cases:
 `[private role="contributor-only"]Text for Contributors only[/private]`
 
 `[private role="subscriber-only"]Text for Subscribers only[/private]`
-
 
 WordPress roles in descending order:
 
@@ -59,7 +58,7 @@ WordPress roles in descending order:
 
  * Subscriber
 
-== Installation ==
+## Installation ##
 
 This section describes how to install the plugin and get it working.
 
@@ -67,9 +66,9 @@ This section describes how to install the plugin and get it working.
 1. Activate the plugin through the Plugins menu in WordPress
 1. Now the shortcode is available and ready to use.
 
-== Frequently Asked Questions ==
+## Frequently Asked Questions ##
 
-= The hidden text is similar to the public text. Is it possible to stylize it in a different look? =
+### The hidden text is similar to the public text. Is it possible to stylize it in a different look? ###
 
 Yes, you have to edit the CSS file of your current theme.
 The shortcode generates a &lt;p&gt; HTML tag with two classes:
@@ -80,41 +79,45 @@ The shortcode generates a &lt;p&gt; HTML tag with two classes:
 
 A third class is added, in case you make a note only for a specific role, for example "contributor-only".
 
-== Screenshots ==
+## Screenshots ##
 
-1. At the center of the screen, the shortcode is used in the WordPress editor. The text inside the shortcode will be displayed only to Authors and above roles.
-2. The shortcode in action. On the left, the text revealed to Administrators only; on the right, the page as seen by lower roles (Editors, Authors, etc., or simply readers).
+### 1. At the center of the screen, the shortcode is used in the WordPress editor. The text inside the shortcode will be displayed only to Authors and above roles. ###
+![1. At the center of the screen, the shortcode is used in the WordPress editor. The text inside the shortcode will be displayed only to Authors and above roles.](http://s-plugins.wordpress.org/private-content/assets/screenshot-1.png)
 
-== Changelog ==
+### 2. The shortcode in action. On the left, the text revealed to Administrators only; on the right, the page as seen by lower roles (Editors, Authors, etc., or simply readers). ###
+![2. The shortcode in action. On the left, the text revealed to Administrators only; on the right, the page as seen by lower roles (Editors, Authors, etc., or simply readers).](http://s-plugins.wordpress.org/private-content/assets/screenshot-2.png)
 
-= 2.0 =
+
+## Changelog ##
+
+### 2.0 ###
 
 * NEW: now you can show a note only to user of a specific role, hiding that note to higher roles.
 * Added uninstall.php to delete the new custom capabilities.
 
-= 1.2 =
+### 1.2 ###
 
 * Now the inline style appears only if necessary.
 
-= 1.1 =
+### 1.1 ###
 
 * Upon request, added the possibility to align the text left, right, centered and justified.
 
-= 1.0 =
+### 1.0 ###
 
 * First release of the plugin.
 
-== Upgrade Notice ==
+## Upgrade Notice ##
 
-= 2.0 =
+### 2.0 ###
 
 Upgrade in order to use the new role-only feature.
 
-= 1.0 =
+### 1.0 ###
 
 No upgrade notice.
 
-== Credits ==
+## Credits ##
 
 Many thanks to:
 
--- private-content.php
+++ private-content.php
@@ -5,12 +5,12 @@
  * Plugin URI: http://www.aldolat.it/wordpress/wordpress-plugins/private-content/
  * Author: Aldo Latino
  * Author URI: http://www.aldolat.it/
- * Version: 1.2
+ * Version: 2.0
  * License: GPLv3 or later
  * Text Domain: private
  * Domain Path: /languages/
  *
- * Copyright (C) 2009, 2012  Aldo Latino  (email : aldolat@gmail.com)
+ * Copyright (C) 2009, 2013  Aldo Latino  (email : aldolat@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * @package PrivateContent
- * @version 1.2
+ * @version 2.0
  * @author Aldo Latino <aldolat@gmail.com>, Jeff Starr
  * @copyright Copyright (c) 2009-2012, Aldo Latino
  * @link http://www.aldolat.it/wordpress/wordpress-plugins/private-content/
@@ -46,11 +46,18 @@
  * [private role="administrator"]Text for administrators[/private]
  * [private role="editor" align="center"]Text for editors[/private]
  * [private role="author"]Text for authors[/private]
+ * [private role="author-only"]Text for authors only[/private]
  * [private role="contributor" align="right"]Text for contributors[/private]
  * [private role="subscriber" align="justify"]Text for subscribers[/private]
+ * [private role="subscriber-only" align="justify"]Text for subscribers only[/private]
  *
  * Please, note that an administrator can read an editor private content or a subscriber private content, and so on.
  * Same thing for editor, author, contributor, and subscriber: a higher role can read a lower role content.
+ *
+ * If you want to show a note only to a certain role, you have to use a <role>-only option.
+ * For example:
+ * [private role="author-only"]Text for authors only[/private]
+ * In this way, Administrators and Editors (roles higher than Editors) can't read this note.
  *
  * WordPress Roles in descending order:
  * Administrator,
@@ -60,6 +67,40 @@
  * Subscriber.
  */
 
+
+/**
+ * Add the new capabilities to WordPress standard roles.
+ * Note that the Administrator role doesn't need any custom capabilities.
+ */
+function ubn_private_add_cap() {
+	global $wp_roles;
+	$wp_roles->add_cap( 'editor',      'read_ubn_editor_notes'      );
+	$wp_roles->add_cap( 'author',      'read_ubn_author_notes'      );
+	$wp_roles->add_cap( 'contributor', 'read_ubn_contributor_notes' );
+	$wp_roles->add_cap( 'subscriber',  'read_ubn_subscriber_notes'  );
+}
+register_activation_hook( __FILE__, 'ubn_private_add_cap' );
+
+
+/*
+ * Check if Editor role has 'read_ubn_editor_notes' capabilities.
+ * This check is useful only when upgrading this plugin from version below 2.0.
+ * This function will be removed in future.
+ */
+function ubn_private_check_capability_exists() {
+	$editor_role = get_role( 'editor' );
+	if ( ! isset( $editor_role->capabilities['read_ubn_editor_notes'] ) ) {
+		ubn_private_add_cap();
+	}
+}
+add_action( 'init', 'ubn_private_check_capability_exists' );
+
+
+/*
+ * Create the shortcode 'private'.
+ *
+ * @usage [private role="role" align="align"]Text to show[/private]
+ */
 function ubn_private_content( $atts, $content = null ) {
 	extract( shortcode_atts( array(
 		'role'  => 'administrator', // The default role if none has been provided
@@ -83,6 +124,9 @@ function ubn_private_content( $atts, $content = null ) {
 			case 'justify' :
 				$align_style = ' style="text-align: justify;"';
 			break;
+
+			default :
+				$align_style = '';
 		}
 	} else {
 		$align_style = '';
@@ -100,9 +144,19 @@ function ubn_private_content( $atts, $content = null ) {
 				$text = '<p class="private editor-content"' . $align_style . '>' . $content . '</p>';
 		break;
 
+		case 'editor-only' :
+			if( current_user_can( 'read_ubn_editor_notes' ) )
+				$text = '<p class="private editor-content editor-only"' . $align_style . '>' . $content . '</p>';
+		break;
+
 		case 'author' :
 			if( current_user_can( 'publish_posts' ) )
 				$text = '<p class="private author-content"' . $align_style . '>' . $content . '</p>';
+		break;
+
+		case 'author-only' :
+			if( current_user_can( 'read_ubn_author_notes' ) )
+				$text = '<p class="private author-content author-only"' . $align_style . '>' . $content . '</p>';
 		break;
 
 		case 'contributor' :
@@ -110,19 +164,31 @@ function ubn_private_content( $atts, $content = null ) {
 				$text = '<p class="private contributor-content"' . $align_style . '>' . $content . '</p>';
 		break;
 
+		case 'contributor-only' :
+			if( current_user_can( 'read_ubn_contributor_notes' ) )
+				$text = '<p class="private contributor-content contributor-only"' . $align_style . '>' . $content . '</p>';
+		break;
+
 		case 'subscriber' :
 			if( current_user_can( 'read' ) )
 				$text = '<p class="private subscriber-content"' . $align_style . '>' . $content . '</p>';
 		break;
 
+		case 'subscriber-only' :
+			if( current_user_can( 'read_ubn_subscriber_notes' ) )
+				$text = '<p class="private subscriber-content subscriber-only"' . $align_style . '>' . $content . '</p>';
+		break;
+
+		default :
+			$text = '<p class="private administrator-content"' . $align_style . '>' . $content . '</p>';
 	}
 
 	if( isset( $text ) )
-		// The "do_shortcode" function is necessary to let WordPress execute another nested shortcode.
+		// The do_shortcode function is necessary to let WordPress execute another nested shortcode.
 		return do_shortcode( $text );
-
 }
 add_shortcode( 'private', 'ubn_private_content' );
+
 
 /***********************************************************************
  *                            CODE IS POETRY
